043576f0f495920fc019be1a7f53eec17df4d21a
JB-38 <joseb@shocklogic.com>
2023-02-15 08:09:08 -0400

    nuevos controladores

--- app/Http/Controllers/AlmacenController.php
+++ app/Http/Controllers/AlmacenController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class ProductController extends Controller
+class AlmacenController extends Controller
 {
     /**
      * Display a listing of the resource.
